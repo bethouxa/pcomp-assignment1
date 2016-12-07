d1b065e90e4c2f73108ec91a1a62e13dd4f11085
Béthoux Antonin <b00093222@student.itb.ie>
2016-12-07 20:41:47 +0000

    Added documentation

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 typedef unsigned int uint;
 typedef unsigned char bool;
 
@@ -29,26 +32,86 @@ typedef struct
 // ######### GRAPH FUNCTIONS ##########
 
 /**
- * Allocates and initializes the graph representation with the correct amount of nodes
+ * Allocates and initializes the graph representation with the correct amount of
+ * nodes
+ *
+ * @param      g        the graph to initialize
+ * @param[in]  nbNodes  The number of nodes
  */
 void allocateAndInitializeACOGraphContents(ACOGraph* g, int nbNodes);
+
+
+/**
+ * @brief      Frees an ACOGrap
+ *
+ * @param      g     the graph to free
+ */
 void freeACOGraph(ACOGraph* g);
-ACOEdge* getEdges(ACOGraph* g, uint nodeId);
+
+/**
+ * @brief      Gets the edges connected to the given node
+ *
+ * @param      g       The ACOgraph we're using
+ * @param[in]  nodeId  The node identifier
+ *
+ * @return     An array of g->nbNodes ACOEdges with NULL values if an edge
+ *             doesn't exist.
+ */	
+ACOEdge** getEdges(ACOGraph* g, uint nodeId);
+
+/**
+ * @brief      Gets the pheromone level between two nodes
+ *
+ * @param      g     The graph we're using
+ * @param[in]  x     One of the nodes connected to the edge
+ * @param[in]  y     The other node connected to the edge
+ *
+ * @return     The pheromone level
+ */
 double getPheromone(ACOGraph* g, uint x, uint y);
+
+/**
+ * @brief      Gets the hive position.
+ *
+ * @param      g     The graph we're using
+ *
+ * @return     The hive position.
+ */
 uint getHivePosition(ACOGraph* g);
+/**
+ * @brief      Determines if the given node is food source.
+ *
+ * @param      g       Graph we're using
+ * @param[in]  nodeId  The node identifier
+ */
 void isFoodSource(ACOGraph* g, uint nodeId);
 
 
 
-
+/**
+ * @brief      Picks the next node for the given ant.
+ * 
+ * This function picks the next node for a given ant. It will compute the probability
+ * of going though each edge using it's pheromone level and weight, then will 
+ * pick an edge through a random weighted choice.
+ *
+ * @param[in]  ant   The ant
+ * @param      g     Graph we're using
+ *
+ * @return     The id of the node where the ant will head next
+ */
 ACOEdge chooseEdges(Ant ant, ACOGraph* g);
 void evaporatePheromones(ACOGraph* g);
 
 /**
- * Move the ant
- * If ant has ever found food update pheromones
- * If ant is on a food source update ant.hasFoudFood
+ * @brief Move the ant to the next node
+ * 
+ * Moves the ant to the next node in it's path (see chooseEdges()), updates pheromones when needed,
+ * updates ant.hasFoundFood if needed.
+ * @param 	ant		The ant to move
+ * @param 	g 		graph we're using
  */
 void move(Ant ant, ACOGraph* g);
 
 
+#endif
